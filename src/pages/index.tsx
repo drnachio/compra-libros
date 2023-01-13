668dcaf3b93173548e0d1b8398bcf4df8bc46505
Ignacio Ferro Picón <nacho@binpar.com>
2023-01-13 16:23:16 +0100

    feat: no user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,11 +21,6 @@ import {
   PhoneIcon,
 } from "@heroicons/react/20/solid";
 
-const user = {
-  name: "Tom Cook",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
   { name: "Inicio", href: "#", icon: HomeIcon, current: false },
   { name: "Autores", href: "#", icon: UserGroupIcon, current: false },
@@ -418,29 +413,6 @@ export default function Example() {
                       </div>
                     </nav>
                   </div>
-                  <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-                    <a href="#" className="group block flex-shrink-0">
-                      <div className="flex items-center">
-                        <div>
-                          <Image
-                            className="inline-block h-10 w-10 rounded-full"
-                            src={user.imageUrl}
-                            alt=""
-                            width={40}
-                            height={40}
-                          />
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                            {user.name}
-                          </p>
-                          <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-                            View profile
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
                 </Dialog.Panel>
               </Transition.Child>
               <div className="w-14 flex-shrink-0" aria-hidden="true">
@@ -513,30 +485,7 @@ export default function Example() {
                     ))}
                   </div>
                 </nav>
-              </div>
-              <div className="flex flex-shrink-0 border-t border-gray-200 p-4">
-                <a href="#" className="group block w-full flex-shrink-0">
-                  <div className="flex items-center">
-                    <div>
-                      <Image
-                        className="inline-block h-9 w-9 rounded-full"
-                        src={user.imageUrl}
-                        width={36}
-                        height={36}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <p className="text-sm font-medium text-gray-700 group-hover:text-gray-900">
-                        {user.name}
-                      </p>
-                      <p className="text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                        View profile
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              </div>
+              </div>              
             </div>
           </div>
         </div>
@@ -546,12 +495,13 @@ export default function Example() {
               <div>
                 <Image
                   className="h-8 w-auto"
-                  src="/logo.svg"
+                  src="/fav.svg"
                   alt="Compra Libros"
                   width={32}
                   height={32}
-                />
+                />                
               </div>
+              <h2 className="text-xl font-bold">Compra Libros</h2>
               <div>
                 <button
                   type="button"
