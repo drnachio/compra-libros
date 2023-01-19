4280747d244099b1adf38c04b476a9af3f1fc896
Ignacio Ferro Picón <nacho@binpar.com>
2023-01-19 19:17:49 +0100

    feat: importe model improved

--- src/tools/importer.ts
+++ src/tools/importer.ts
@@ -6,6 +6,17 @@ dotenv.config();
 
 const logSkippedFiles = false;
 
+const getDateFromMARCNumber = (dateNumber: number): Date => {
+  const stringDate = `${dateNumber}`;
+  return new Date(
+    stringDate.substring(0, 4) +
+      '-' +
+      stringDate.substring(4, 6) +
+      '-' +
+      stringDate.substring(6),
+  );
+};
+
 const contentImporter = async (): Promise<void> => {
   const files = await fs.readdir('data');
   let totalProcessed = 0;
@@ -24,6 +35,80 @@ const contentImporter = async (): Promise<void> => {
           data.Contributor = [
             data.Contributor as unknown,
           ] as MARKMetadata['Contributor'];
+        }
+        if (data.Language && !Array.isArray(data.Language)) {
+          data.Language = [
+            data.Language as unknown,
+          ] as MARKMetadata['Language'];
+        }
+        if (data.Extent && !Array.isArray(data.Extent)) {
+          data.Extent = [data.Extent as unknown] as MARKMetadata['Extent'];
+        }
+        if (data.Subject && !Array.isArray(data.Subject)) {
+          data.Subject = [data.Subject as unknown] as MARKMetadata['Subject'];
+        }
+        if (data.OtherText && !Array.isArray(data.OtherText)) {
+          data.OtherText = [
+            data.OtherText as unknown,
+          ] as MARKMetadata['OtherText'];
+        }
+        if (data.Publisher && !Array.isArray(data.Publisher)) {
+          data.Publisher = [
+            data.Publisher as unknown,
+          ] as MARKMetadata['Publisher'];
+        }
+        if (data.Publisher) {
+          data.Publisher.forEach((publisher) => {
+            if (publisher.Website && !Array.isArray(publisher.Website)) {
+              publisher.Website = [
+                publisher.Website as unknown,
+              ] as MARKMetadata['Publisher'][number]['Website'];
+            }
+          });
+        }
+        if (data.MediaFile && !Array.isArray(data.MediaFile)) {
+          data.MediaFile = [
+            data.MediaFile as unknown,
+          ] as MARKMetadata['MediaFile'];
+        }
+        if (data.SalesRights && !Array.isArray(data.SalesRights)) {
+          data.SalesRights = [
+            data.SalesRights as unknown,
+          ] as MARKMetadata['SalesRights'];
+        }
+        if (data.Measure && !Array.isArray(data.Measure)) {
+          data.Measure = [data.Measure as unknown] as MARKMetadata['Measure'];
+        }
+        if (data.RelatedProduct && !Array.isArray(data.RelatedProduct)) {
+          data.RelatedProduct = [
+            data.RelatedProduct as unknown,
+          ] as MARKMetadata['RelatedProduct'];
+        }
+        if (data.RelatedProduct) {
+          data.RelatedProduct.forEach((relatedProduct) => {
+            if (
+              relatedProduct.ProductIdentifier &&
+              !Array.isArray(relatedProduct.ProductIdentifier)
+            ) {
+              relatedProduct.ProductIdentifier = [
+                relatedProduct.ProductIdentifier as unknown,
+              ] as MARKMetadata['RelatedProduct'][number]['ProductIdentifier'];
+            }
+          });
+        }
+        if (data.SupplyDetail && !Array.isArray(data.SupplyDetail)) {
+          data.SupplyDetail = [
+            data.SupplyDetail as unknown,
+          ] as MARKMetadata['SupplyDetail'];
+        }
+        if (data.SupplyDetail) {
+          data.SupplyDetail.forEach((supply) => {
+            if (supply.Price && !Array.isArray(supply.Price)) {
+              supply.Price = [
+                supply.Price as unknown,
+              ] as MARKMetadata['SupplyDetail'][number]['Price'];
+            }
+          });
         }
         if (data.Title) {
           if (data.Title && !Array.isArray(data.Title)) {
@@ -57,9 +142,49 @@ const contentImporter = async (): Promise<void> => {
               if (
                 data.MainSubject.some((c) => `${c.SubjectCode}`.startsWith('F'))
               ) {
-                if (!data.Contributor) {
+                if (!data.Contributor || data.Contributor.length === 0) {
                   logSkippedFiles &&
                     console.error(`No contributor in ISBN ${file || ''}`);
+                } else if (!data.Contributor[0]?.BiographicalNote) {
+                  logSkippedFiles &&
+                    console.error(
+                      `No Bio For Contributor File in ISBN ${file || ''}`,
+                    );
+                } else if (!data.MediaFile || data.MediaFile.length === 0) {
+                  logSkippedFiles &&
+                    console.error(`No Media File in ISBN ${file || ''}`);
+                } else if (!data.OtherText || data.OtherText.length === 0) {
+                  logSkippedFiles &&
+                    console.error(`No OtherText in ISBN ${file || ''}`);
+                } else if (!data.Publisher || data.Publisher.length === 0) {
+                  logSkippedFiles &&
+                    console.error(`No Publisher in ISBN ${file || ''}`);
+                } else if (
+                  !data.SupplyDetail ||
+                  data.SupplyDetail.length === 0
+                ) {
+                  logSkippedFiles &&
+                    console.error(`No SupplyDetail in ISBN ${file || ''}`);
+                } else if (data.PublishingStatus !== 4) {
+                  logSkippedFiles &&
+                    console.error(`Not published in ISBN ${file || ''}`);
+                } else if (!data.PublicationDate) {
+                  logSkippedFiles &&
+                    console.error(`No publication date in ISBN ${file || ''}`);
+                } else if (
+                  getDateFromMARCNumber(data.PublicationDate) > new Date()
+                ) {
+                  logSkippedFiles &&
+                    console.error(`Not published ISBN ${file || ''}`);
+                } else if (
+                  data.SupplyDetail.find(
+                    (c) =>
+                      c.SupplyToCountry &&
+                      c.SupplyToCountry.indexOf('ES') !== -1,
+                  )?.Price.length === 0
+                ) {
+                  logSkippedFiles &&
+                    console.error(`No price for ES in ISBN ${file || ''}`);
                 } else {
                   console.log(`Processing book ${file || ''}: ${mainTitle}`);
                   totalProcessed++;
--- src/tools/markTypes.ts
+++ src/tools/markTypes.ts
@@ -6,6 +6,10 @@ export interface MARKMetadata {
     IDValue: number;
   }[];
   ProductForm: string;
+  ProductFormDetail: string;
+  ProductFormFeature: {
+    ProductFormFeatureType: number;
+  };
   Series: {
     Title: {
       TitleType: number;
@@ -30,9 +34,92 @@ export interface MARKMetadata {
       IDValue: number;
     }[];
   }[];
+  NoEdition: string;
+  Language: {
+    LanguageRole: number;
+    LanguageCode: string;
+  }[];
+  NumberOfPages: number;
+  Extent: {
+    ExtentType: number;
+    ExtentValue: number;
+    ExtentUnit: number;
+  }[];
   MainSubject: {
     MainSubjectSchemeIdentifier: number;
     SubjectSchemeVersion: number;
     SubjectCode: string;
+  }[];
+  Subject: {
+    SubjectSchemeIdentifier: number;
+    SubjectSchemeName: string;
+    SubjectCode: number;
+    SubjectHeadingText: string;
+  }[];
+  AudienceCode: number;
+  OtherText: {
+    TextTypeCode: number;
+    Text: string;
+  }[];
+  MediaFile: {
+    MediaFileTypeCode: number;
+    MediaFileFormatCode?: number;
+    MediaFileLinkTypeCode: number;
+    MediaFileLink: string;
+    MediaFileDate: number;
+  }[];
+  Imprint: {
+    NameCodeType?: number;
+    NameCodeTypeName?: string;
+    NameCodeValue?: string;
+    ImprintName: string;
+  }[];
+  Publisher: {
+    PublishingRole: number;
+    NameCodeType: number;
+    NameCodeTypeName: string;
+    NameCodeValue: string;
+    PublisherName: string;
+    Website?: {
+      WebsiteRole: number;
+      WebsiteLink: string;
+    }[];
+  }[];
+  CityOfPublication?: string;
+  CountryOfPublication?: string;
+  PublishingStatus: number;
+  PublicationDate: number;
+  SalesRights: {
+    SalesRightsType: number;
+    RightsCountry: string;
+  }[];
+  Measure: {
+    MeasureTypeCode: number,
+    Measurement: number,
+    MeasureUnitCode: string;
+  }[];
+  RelatedProduct: {
+    RelationCode: number;
+    ProductIdentifier: {
+      ProductIDType: number;
+      IDValue: number;
+    }[];
+  }[];
+  SupplyDetail: {
+    SupplierName: string;
+    SupplierRole: number;
+    SupplyToCountry: string;
+    SupplyRestrictionDetail: string;
+    ProductAvailability: number;
+    OnSaleDate: number;
+    Price: {
+      PriceTypeCode: number,
+      PriceAmount: number,
+      CurrencyCode: string,
+      CountryCode: string,
+      TaxRateCode1: string,
+      TaxRatePercent1: number,
+      TaxableAmount1: number,
+    }[]
   }[];
 }
